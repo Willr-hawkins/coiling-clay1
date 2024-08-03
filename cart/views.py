bf1e6a944fd83fb00fcde336d09b8d12ddd10edf
William Hawkins <Hawkinswill02@gmail.com>
2024-08-03 11:37:08 +0000

    Added update and remove from bag functionality

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -21,3 +21,30 @@ def add_to_cart(request, item_id):
     
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+def adjust_cart(request, item_id):
+    """ A view to allow the cart items to be adjusted. """
+
+    qauntity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if qauntity > 0:
+        cart[item_id] = qauntity
+    else:
+        cart.pop(item_id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('cart'))
+
+def remove_from_cart(request, item_id):
+    """ A view to allow the cart items to be removed. """
+
+    try:
+        cart = request.session.get('cart', {})
+
+        cart.pop(item_id)
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)

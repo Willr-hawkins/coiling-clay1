f3422362f974e39cda83951b169da54e856a6b2c
William Hawkins <Hawkinswill02@gmail.com>
2024-08-08 16:07:39 +0000

    Completed webhook handlers

--- checkout/admin.py
+++ checkout/admin.py
@@ -13,14 +13,16 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_number', 'date', 
                         'shipping_cost', 'order_total', 
-                        'grand_total',)
+                        'grand_total', 'original_cart',
+                        'stripe_pid',)
 
     fields = ('order_number', 'date', 'full_name', 'email',
                 'phone_number', 'country', 'postcode',
                 'town_or_city', 'street_address1', 
                 'street_address2', 'county', 
                 'shipping_cost', 'order_total', 
-                'grand_total',)
+                'grand_total', 'original_cart',
+                'stripe_pid',)
 
     list_display = ('order_number', 'date', 'full_name', 
                     'shipping_cost', 'order_total', 

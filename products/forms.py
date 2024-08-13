a5058ef7bc50bf11ac8bf97bd3813a5927b0f4b5
William Hawkins <Hawkinswill02@gmail.com>
2024-08-13 13:45:10 +0000

    Finsihed the add product functionality

--- products/forms.py
+++ products/forms.py
@@ -15,4 +15,5 @@ class ProductForm(forms.ModelForm):
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-2'
+            self.fields['image_url'].required = True
 

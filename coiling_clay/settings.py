a2967b5e58bd3a7640c0ca428663e747abbd4f00
William Hawkins <Hawkinswill02@gmail.com>
2024-08-15 12:35:06 +0000

    Fix deployment issues

--- coiling_clay/settings.py
+++ coiling_clay/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '1oq%k81a7jd2oxy!8ok9v@xzovpnodyw*2n+u_yqav(9z793es')
+SECRET_KEY = True #os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
